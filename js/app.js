d26c210070fcfb1a77e08552d9c3dbbca9c702cd
Pedro Duarte <pdjtresr431@gmail.com>
2025-05-20 20:24:01 -0300

    primeiro commit

---
+++ js/app.js
@@ -0,0 +1,16 @@
+let pessoa = [];
+
+function adicionar(){
+    let amigo = document.getElementById('nome-amigo').value;
+    pessoa.push(amigo);
+    
+    console.log(pessoa);
+}
+
+/*function sortear() {
+    
+}
+
+function reiniciar() {
+    
+}*/
